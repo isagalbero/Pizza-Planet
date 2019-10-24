e294284daa4062020c36716b48aae493cce3c660
Isabella Galbero <isagalbero@gmail.com>
2019-10-24 11:41:45 -0300

    Validação de formulário com Regex para título e ingredientes e função filter_var para email

--- add.php
+++ add.php
@@ -15,23 +15,33 @@
 
     //check email if it's empty
     if(empty($_POST['email'])){
-      echo 'An email is required <br />'
+      echo 'An email is required <br />';
     } else {
-      echo htmlspecialchars($_POST['email']);
+      $email = $_POST['email'];
+      if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        echo 'must be a valid email';
+      }
     }
     
     //check title
     if(empty($_POST['title'])){
-      echo 'Title is empty <br />'
+      echo 'Title is empty <br />';
     } else {
-      echo htmlspecialchars($_POST['title']);
+      $title = $_POST['title'];
+      if(!preg_match('/^[a-zA-Z\+]$/', $title)){
+        echo 'Title must be letters and spaces only';
+      }
     }
     
     //check ingredients
     if(empty($_POST['ingredients'])){
-      echo 'Ingredients is empty <br />'
+      echo 'Ingredients is empty <br />';
     } else {
-      echo htmlspecialchars($_POST['ingredients']);
+      //echo htmlspecialchars($_POST['ingredients']);
+      $ingredients = $_POST['ingredients'];
+      if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
+        echo 'Ingredients must be a comma separated list';
+      }
     }
   }
 
