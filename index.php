b832c4cfa84e19b287bde51b07cd7f8222b3b91e
Isabella Galbero <isagalbero@gmail.com>
2019-10-29 09:14:31 -0300

    Mudando a sintaxe de '{}' para ': endforeach; endif;'

--- index.php
+++ index.php
@@ -31,15 +31,15 @@
 <div class="container">
   <div class="row">
 
-    <?php foreach($pizzas as $pizza){ ?>
+    <?php foreach($pizzas as $pizza): ?>
       <div class="col s6 md3">
         <div class="card z-depth-0">
           <div class="card-content center">
             <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
             <ul>
-              <?php foreach(explode(',', $pizza['ingredients']) as $ing){ ?>
+              <?php foreach(explode(',', $pizza['ingredients']) as $ing): ?>
                 <li><?php echo htmlspecialchars($ing); ?></li>
-              <?php } ?>
+              <?php endforeach; ?>
             </ul>
           </div>
           <div class="card-action right-align">
@@ -47,7 +47,7 @@
           </div>
         </div>
       </div>
-    <?php } ?>
+    <?php endforeach; ?>
 
   </div>
 </div>
